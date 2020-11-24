a8fea4436683b55b506fe69ca9f1f27141996854
unknown <cristian.saranguiz@gmail.com>
2020-11-24 17:12:51 -0300

    init bin

---
+++ Tarea2/binario.py
@@ -0,0 +1,26 @@
+import random
+class GA_binary:
+    def __init__(number_to_convert,number_genes):
+        self.n_convert=number_to_convert
+        self.n_genes=number_genes
+    def fitness_bits(self, indv):
+        result=0
+        exp = 1
+        for i in indv[::-1]:
+            result += int(v) + exp
+            exp *= 2
+        return - abs(self.n_convert - result )
+    def gen_factory():
+        if(random.random() > 0.5):
+            return '1'
+        else:
+            return '0'
+    def sequence_bit_factory(self):
+        r=[]
+        for i in range(self.n_genes):
+            r.append(gen_factory()) 
+        return r
+    def runGA(self):
+        ga= GA(pop_size,mutation_rate=0.1,fitness=fitness_bits,individual_factory= self.sequence_bit_factory(),gene_factory=self.gen_factory(),termination_condition = lambda f : f == 0, silent =False,max_iter=10)
+        best_fitness_list,avg_list,best_indv = ga.run()
+        print(''.join(best_individual))
