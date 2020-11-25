39f7b082428e328ec5e46005c3c9879fb24b3587
unknown <cristian.saranguiz@gmail.com>
2020-11-25 15:33:09 -0300

    delete seed

--- Tarea2/GA/Selector/Roulette.py
+++ Tarea2/GA/Selector/Roulette.py
@@ -7,7 +7,6 @@ class Roulette:
     def run(self,individuals,semilla):
         f= self.fitness_list(individuals)
         fsum=self.sum_fitness_list(f)
-        rand= np.random.seed(semilla)
         selected = rand.randint(fsum + 1)
         sIndividual= self.selected_individuals(f,individuals,selected)
         return sIndividual
